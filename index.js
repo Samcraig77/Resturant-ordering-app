0d612cb9e96eee369e88d006a567dede19cf4fe6
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-08-08 16:37:26 -0700

    Boiler Plate for app

---
+++ index.js
@@ -0,0 +1,23 @@
+import { menuArray } from "./data";
+
+const menuArea = document.getElementById('menu')
+const orderArea = document.getElementById('order-area')
+
+// function getMenu() {
+//     menu = ''
+//     menuArray.forEach(dish => {
+//         menu += `
+//         pass the ${dish.ingredients}
+//         `
+//     }`
+// )
+//     return menu;
+// }
+
+// function renderMenu() {
+//     menuArea.innerHTML = getMenu()
+// }
+
+// renderMenu()
+
+console.log("hell its about time")
