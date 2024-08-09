f9a5e0e5996558c93728b0b6a6724c5e80d3d4dc
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-08-08 23:08:17 -0700

    Function to render menu items set
    Todo: function flow for adding/removing items to/from cart and bringing up pay modal

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-import { menuArray } from "./data";
+import { menuArray } from "./data.js"
 
 const menuArea = document.getElementById('menu')
-const orderArea = document.getElementById('order-area')
+// const orderArea = document.getElementById('order-area')
 
-// function getMenu() {
-//     menu = ''
-//     menuArray.forEach(dish => {
-//         menu += `
-//         pass the ${dish.ingredients}
-//         `
-//     }`
-// )
-//     return menu;
-// }
 
-// function renderMenu() {
-//     menuArea.innerHTML = getMenu()
-// }
 
-// renderMenu()
 
-console.log("hell its about time")
+function getMenu() {
+    let menu = ''
+    menuArray.forEach(item => {
+        menu += `
+        <li class="menu-item" id="${item.name}">
+
+            <div class="emoji-div"> ${item.emoji} </div>
+                <div class="item-info">
+                    <hgroup>
+                        <h3 class="item-name"> ${item.name} </h3>
+                        <p class="ingredients"> ${item.ingredients.join(', ')} </p>
+                        <h3 class="item-price"> $${item.price} </h3>
+                    </hgroup>
+                </div>
+
+            <div class="btn-container">
+                <button class="add-btn" id="">+</button>
+            </div>
+        </li>
+        `
+    })
+    return menu
+}
+
+function renderMenu() {
+    menuArea.innerHTML = getMenu()
+}
+
+renderMenu()
+
+// console.log(menuArray)
