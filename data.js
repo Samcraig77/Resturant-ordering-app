9de9fa1d1fc63de3b8363194cfa7a22c41c0166d
Samcraig77 <SAMCRAIG70@GMAIL.COM>
2024-08-14 20:10:43 -0700

    changed item prices to strings

--- data.js
+++ data.js
@@ -2,21 +2,21 @@ export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushroom", "mozzarella"],
-        price: 14,
+        price: '14.00',
         emoji: "🍕",
         id: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
+        price: '12.00',
         emoji: "🍔",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
-        price: 12,
+        price: '12.00',
         emoji: "🍺",
         id: 2
     }
