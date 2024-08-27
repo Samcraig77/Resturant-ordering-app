f40a8f498471ece4008513cd6f3ad6d3a51d125c
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-08-27 15:03:45 -0700

    Small changes for item height and code readability
    aria label added to the "add-btn as well

--- index.js
+++ index.js
@@ -109,24 +109,27 @@ function renderCheckoutArea(){
     `
 }
 
-// Retrieve menu
+// Retrieve menuArray items from data.js
 function getMenu() {
     let menu = ''
     menuArray.forEach(item => {
         menu += `
         <li class="menu-item" id="${item.name}">
 
-            <div class="emoji-div"> ${item.emoji} </div>
-                <div class="item-info">
-                    <hgroup>
-                        <h3 class="item-name"> ${item.name} </h3>
-                        <p class="ingredients"> ${item.ingredients.join(', ')} </p>
-                        <h3> $${item.price} + Tax</h3>
-                    </hgroup>
-                </div>
+            <div class="emoji-div" aria-hidden="true"> 
+                ${item.emoji} 
+            </div>
+
+            <div class="item-info">
+                <hgroup>
+                    <h3 class="item-name"> ${item.name} </h3>
+                    <p class="ingredients"> ${item.ingredients.join(', ')} </p>
+                    <h3> $${item.price} + Tax</h3>
+                </hgroup>
+            </div>
 
             <div class="btn-container">
-                <button class="add-btn" data-add="${item.id}">+</button>
+                <button class="add-btn" data-add="${item.id}" aria-label="Add item">+</button>
             </div>
         </li>`
     })
